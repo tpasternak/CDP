dc67688948ea8c184e24f6cdbdcdca3b13e5b58c
tomasz <pasternak.t@gmail.com>
2014-07-19 19:08:01 +0200

    First few tests.

--- MOA/ut/tests.cpp
+++ MOA/ut/tests.cpp
@@ -9,33 +9,39 @@ using boost::property_tree::ptree;
 using boost::property_tree::read_json;
 using namespace std;
 
-using mapping2dto3d =
-    vector<pair<tuple<int, int>, tuple<double, double, double>>>;
+using mapping2dto3dEntry = pair<tuple<int, int>, tuple<double, double, double>>;
 
-mapping2dto3d getMapping(const string &jsonInput) {
-  decltype(getMapping("")) answer;
-  stringstream ss;
+using mapping2dto3d = vector<mapping2dto3dEntry>;
+
+tuple<int, int> get2dCoordinates(ptree::value_type const &entry) {
+  int x2d = entry.second.get<int>("2d-X");
+  int y2d = entry.second.get<int>("2d-Y");
+  return make_tuple(x2d, y2d);
+}
+
+mapping2dto3d getMapping(istream &jsonInput);
+mapping2dto3d getMapping(istream &jsonInput) {
+  mapping2dto3d answer;
   ptree tree;
-  ss << jsonInput;
-  read_json(ss, tree);
+  read_json(jsonInput, tree);
   for (const auto &mapping : tree.get_child("mapping")) {
-    int x2d = mapping.second.get<int>("2d-X");
-    int y2d = mapping.second.get<int>("2d-Y");
+    auto coordinates2d = get2dCoordinates(mapping);
     int x3d = mapping.second.get<int>("3d-X");
     int y3d = mapping.second.get<int>("3d-Y");
     int z3d = mapping.second.get<int>("3d-Z");
-    answer.emplace_back(
-        make_pair(make_tuple(x2d, y2d), make_tuple(x3d, y3d, z3d)));
+    get2dCoordinates(mapping);
+    answer.emplace_back(make_pair(coordinates2d, make_tuple(x3d, y3d, z3d)));
   }
-
   return answer;
 }
 TEST(Mapping, EmptyMapping) {
   // Arrange
   const string emptyMappingString = "{  \"mapping\":[] }";
+  stringstream emptyMappingStream;
+  emptyMappingStream << emptyMappingString;
 
   // Act
-  auto mapping = getMapping(emptyMappingString);
+  auto mapping = getMapping(emptyMappingStream);
 
   // Assert
   ASSERT_EQ(0, mapping.size());
@@ -56,11 +62,11 @@ TEST(Mapping, BasicMapping) {
       "               }                                "
       "           ]                                    "
       "       }                                        ";
-  stringstream basicMappingSream;
-  basicMappingSream << basicMappingString;
+  stringstream basicMappingStream;
+  basicMappingStream << basicMappingString;
 
   // Act
-  const auto mapping = getMapping(basicMappingString);
+  const auto mapping = getMapping(basicMappingStream);
 
   // Assert
   ASSERT_EQ(make_tuple(9, 6), mapping[0].first);
